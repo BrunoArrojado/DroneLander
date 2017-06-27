e8877ab6bbef534c178d8697d7770599fd5b4e44
Bruno Arrojado <b.arrojado@gmail.com>
2017-06-27 12:27:57 +0100

    das

--- DroneLander/DroneLander/Common/CoreConstants.cs
+++ DroneLander/DroneLander/Common/CoreConstants.cs
@@ -17,4 +17,10 @@ namespace DroneLander.Common
         public const double StartingFuel = 1000.0;
         public const double StartingThrust = 0.0;
     }
+
+    public static class MobileCenterConstants
+    {
+        public const string AndroidAppId = "429b73e3-8379-4886-91cd-912252c51545";
+        public const string iOSAppId = "";
+    }
 }
